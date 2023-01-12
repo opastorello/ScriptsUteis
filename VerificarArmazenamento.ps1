fc116334a5aea8930312e875493b5132ee1aac94
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2023-01-12 01:11:25 -0300

    Update VerificarArmazenamento.ps1

--- VerificarArmazenamento.ps1
+++ VerificarArmazenamento.ps1
@@ -1,12 +1,18 @@
 # Define a lista de servidores
 $servers = "Server1", "Server2", "Server3"
 
-# Solicita credenciais do usuário
+#Solicita credenciais do usuário
 $credentials = Get-Credential
 
 # Loop através da lista de servidores
 foreach ($server in $servers) {
     try {
+        # Loop enquanto credenciais estão inválidas
+        do {        
+            # Testa as credenciais
+            $connection = Test-WsMan -ComputerName $server -Credential $credentials -ErrorAction Stop
+        } while (!$connection)
+        
         # Obter informações de armazenamento do servidor atual usando as credenciais do usuário
         $storage = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $server -Credential $credentials -Filter "DeviceID='C:'"
 
