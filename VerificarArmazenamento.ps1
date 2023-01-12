5c783a413f107bf970f4420cba2cd9e21bac2dab
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2023-01-12 01:07:20 -0300

    Create VerificarArmazenamento.ps1

---
+++ VerificarArmazenamento.ps1
@@ -0,0 +1,20 @@
+# Define a lista de servidores
+$servers = "Server1", "Server2", "Server3"
+
+# Solicita credenciais do usuário
+$credentials = Get-Credential
+
+# Loop através da lista de servidores
+foreach ($server in $servers) {
+    try {
+        # Obter informações de armazenamento do servidor atual usando as credenciais do usuário
+        $storage = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $server -Credential $credentials -Filter "DeviceID='C:'"
+
+        # Exibir o nome do servidor e o espaço de armazenamento disponível
+        Write-Host "Servidor: $server`nArmazenamento disponível: $(($storage.FreeSpace/1GB)) GB"
+    }
+    catch {
+        # Exibir uma mensagem de erro se não for possível obter informações do servidor
+        Write-Host "Não é possível obter informações de armazenamento para $server. Erro: $($_.Exception.Message)"
+    }
+}
